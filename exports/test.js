2f2cd6d4b9b58c4c1ff804b8322c7d999e22ccb7
N-a-4 <159144156+N-a-4@users.noreply.github.com>
2025-09-15 16:40:15 +0000

    dev 20

---
+++ exports/test.js
@@ -0,0 +1,22 @@
+const exampleContainer = new ContainerBuilder()
+  .addMediaGalleryComponents(mediaGallery => mediaGallery
+    .addItems(
+      new MediaGalleryItemBuilder().setURL("https://i.ibb.co/gM3ZJYGt/vidget.png?ex=689d27e1&is=689bd661&hm=0ba370ab75ace8478cbcc6c596b0dda51c9a9dc41b055f881c3ef83371f7e094&=&format=webp&quality=lossless&width=1100&height=330")
+    )
+  )
+  .addActionRowComponents(row => row
+    .addComponents(
+      new ButtonBuilder().setLabel("Кнопка").setCustomId("btn:b1").setStyle(ButtonStyle.Secondary)
+    )
+  )
+  .addSeparatorComponents(separator => separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large))
+  .addSectionComponents(section => section
+    .addTextDisplayComponents(textDisplay => textDisplay.setContent(`# Заголовок
+
+Текст с кнопкой справа.`))
+    .setButtonAccessory(btn => btn.setLabel("Тестовая кнопка").setCustomId("btn:btn-1757954374671").setStyle(ButtonStyle.Secondary))
+  )
+await interaction.editReply({
+  flags: MessageFlags.IsComponentsV2,
+  components: [exampleContainer]
+})
