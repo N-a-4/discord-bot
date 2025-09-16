4ac7645a488d96f1a3410b5b5df4e332b93e864e
N-a-4 <159144156+N-a-4@users.noreply.github.com>
2025-09-16 14:30:46 +0000

    dev 20

--- exports/test.js
+++ exports/test.js
@@ -1,47 +1,18 @@
 const exampleContainer = new ContainerBuilder()
   .addMediaGalleryComponents(mediaGallery => mediaGallery
     .addItems(
-      new MediaGalleryItemBuilder().setURL("https://images-ext-1.discordapp.net/external/Zo1cr39BcZ0zkE7QP9rHwQRfIrst12Amd0eXiSS3AU0/https/i.postimg.cc/sDLZy6wd/user-banner23.png?format=webp&quality=lossless&width=825&height=330")
+      new MediaGalleryItemBuilder().setURL("https://i.ibb.co/gM3ZJYGt/vidget.png?ex=689d27e1&is=689bd661&hm=0ba370ab75ace8478cbcc6c596b0dda51c9a9dc41b055f881c3ef83371f7e094&=&format=webp&quality=lossless&width=1100&height=330")
     )
   )
   .addActionRowComponents(row => row
     .addComponents(
-      new ButtonBuilder().setCustomId("embed:sub-1755357504688-2489").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.back_purp.id }),
-      new ButtonBuilder().setLabel("BiG").setCustomId("btn:btn-1tzbrb").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.crown_mix.id })
+      new ButtonBuilder().setLabel("Кнопка").setCustomId("btn:b1").setStyle(ButtonStyle.Secondary)
     )
   )
-  .addSeparatorComponents(separator => separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large))
   .addSectionComponents(section => section
-    .addTextDisplayComponents(textDisplay => textDisplay.setContent(`${emojis.crown_mix} **Клан**: **[BiG](https:///adasd.com/)**
-${emojis.user_2_mix} **Игрок**: @username (\`.na4.\`)
-${emojis.mic_small_gray} \`Общение\`: 364ч.
-${emojis.game_small_gray} \`Наиграно\`: 145ч`))
-    .setThumbnailAccessory(thumb => thumb.setURL("https://i.ibb.co/m5Zj2zJf/ava-static.png"))
-  )
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(textDisplay => textDisplay.setContent(`${emojis.lin1} **Пробыл**: 43 дня
-${emojis.a2} **Причина ухода**: По собственному желанию`))
-    .setButtonAccessory(btn => btn.setLabel("Профиль клана").setCustomId("embed:embed-1754860281662").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.crown_mix.id }))
-  )
-  .addSeparatorComponents(separator => separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large))
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(textDisplay => textDisplay.setContent(`### Время пребывания на должностях`))
-  )
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(textDisplay => textDisplay.setContent(`${emojis.position} **Глава клана**
-${emojis.dot_purp} Руководит кланом, принимает ключевые решения`))
-    .setButtonAccessory(btn => btn.setLabel("0 дней").setCustomId("btn:btn-1756053395927").setStyle(ButtonStyle.Secondary))
-  )
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(textDisplay => textDisplay.setContent(`${emojis.position} **Заместитель**
-${emojis.dot_purp} Помогает Главе управлять кланом и принимает решения в его отсутствии`))
-    .setButtonAccessory(btn => btn.setLabel("0 дней").setCustomId("btn:btn-1756053621278").setStyle(ButtonStyle.Secondary))
-  )
-  .addActionRowComponents(row => row
-    .addComponents(
-      new ButtonBuilder().setLabel("Роли").setCustomId("mini:mini-cnwhrpj").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.role_mask_mix.id }),
-      new ButtonBuilder().setLabel("Должности").setCustomId("btn:btn-1757941770768").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.position.id })
-    )
+    .addTextDisplayComponents(textDisplay => textDisplay.setContent(`This text is inside a Text Display component! You can use **any __markdown__** available inside this component too.
+And you can place one button or one thumbnail component next to it!`))
+    .setButtonAccessory(btn => btn.setLabel("Button inside a Section").setCustomId("btn:btn-1758032976941").setStyle(ButtonStyle.Secondary))
   )
 await interaction.editReply({
   flags: MessageFlags.IsComponentsV2,
