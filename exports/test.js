c0eb14645ef0afce35eaf85ddf67c1fa1b1b7cc0
N-a-4 <159144156+N-a-4@users.noreply.github.com>
2025-09-16 14:41:33 +0000

    dev 20

--- exports/test.js
+++ exports/test.js
@@ -6,13 +6,13 @@ const exampleContainer = new ContainerBuilder()
   )
   .addActionRowComponents(row => row
     .addComponents(
-      new ButtonBuilder().setLabel("Кнопка").setCustomId("btn:b1").setStyle(ButtonStyle.Secondary)
+      new ButtonBuilder().setLabel("Кнопка").setCustomId("btn:b1").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.crown_mix.id })
     )
   )
   .addSectionComponents(section => section
     .addTextDisplayComponents(textDisplay => textDisplay.setContent(`This text is inside a Text Display component! You can use **any __markdown__** available inside this component too.
 And you can place one button or one thumbnail component next to it!`))
-    .setButtonAccessory(btn => btn.setLabel("Button inside a Section").setCustomId("btn:btn-1758033482148").setStyle(ButtonStyle.Secondary))
+    .setButtonAccessory(btn => btn.setLabel("Button inside a Section").setCustomId("btn:btn-1758033482148").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.handshake_mix.id }))
   )
 await interaction.editReply({
   flags: MessageFlags.IsComponentsV2,
