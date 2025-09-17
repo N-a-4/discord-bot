bf3c1e7e76ae29a166c7d8af4356a5d117f2e491
N-a-4 <159144156+N-a-4@users.noreply.github.com>
2025-09-17 15:35:45 +0000

    dev 20

--- exports/profile_player.js
+++ exports/profile_player.js
@@ -1,51 +1,74 @@
 const exampleContainer = new ContainerBuilder()
   .addMediaGalleryComponents(mediaGallery => mediaGallery
     .addItems(
-      new MediaGalleryItemBuilder().setURL("https://images-ext-1.discordapp.net/external/Zo1cr39BcZ0zkE7QP9rHwQRfIrst12Amd0eXiSS3AU0/https/i.postimg.cc/sDLZy6wd/user-banner23.png?format=webp&quality=lossless&width=825&height=330")
+      new MediaGalleryItemBuilder().setURL("https://rustify.s3.eu-central-003.backblazeb2.com/web_banner_2.gif")
     )
+  )
+  .addSectionComponents(section => section
+    .addTextDisplayComponents(
+      textDisplay => textDisplay
+        .setContent(`### Поддержка
+${emojis.info_yell} Если у вас возникли **трудности с освоением** функционала либо иной **другой вопрос**, связанный с проектом **Rustify** — наша **служба поддержки** всегда готова вам помочь!`),
+    )
+    .setThumbnailAccessory(thumb => thumb.setURL("https://cdn.discordapp.com/emojis/1395320346869104721.webp?size=128&animated=true"))
   )
   .addActionRowComponents(row => row
     .addComponents(
-      new ButtonBuilder().setLabel("BiG3").setCustomId("btn:btn-1754860305089").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.crown_mix.id }).setDisabled(true),
-      new ButtonBuilder().setLabel("Игроки").setCustomId("embed:sub-1754942674709-psz9").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.members_mix.id }),
-      new ButtonBuilder().setLabel("Группы").setCustomId("btn:btn-1758118590500").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.clanstaff.id })
+      new ButtonBuilder().setLabel("Задать вопрос").setCustomId("modal:m_lqwqxe6").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.chat.id }),
+      new ButtonBuilder().setLabel("Жалоба").setCustomId("mini:mini-tl1tc1a").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.report.id })
     )
   )
   .addSeparatorComponents(separator => separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large))
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(
-      textDisplay => textDisplay
-        .setContent(`${emojis.crown_mix} **Клан**: **[BiG](https:///adasd.com/)**
-${emojis.members} \`Состав\`: 36 игроков
-${emojis.rating} \`Рейтинг\`: [ 6.8 / 10.0 ]
-${emojis.bank} \`Банк\`: Скоро`),
-    )
-    .setThumbnailAccessory(thumb => thumb.setURL("https://i.ibb.co/m5Zj2zJf/ava-static.png"))
+  .addTextDisplayComponents(
+    textDisplay => textDisplay
+      .setContent('### Ваши обращения'),
   )
   .addSectionComponents(section => section
     .addTextDisplayComponents(
       textDisplay => textDisplay
-        .setContent(`${emojis.bigdot_yell} **Голосовое общение**
-${emojis.dot_yell} Основная комната для переговоров`),
+        .setContent(`${emojis.chat} **Вопрос #571**
+${emojis.dot_yell} Тикет открыт: \`26/04/25\``),
     )
-    .setButtonAccessory(btn => btn.setLabel("Подключиться").setCustomId("mini:mini-g33re7j").setStyle(ButtonStyle.Secondary))
-  )
-  .addSeparatorComponents(separator => separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large))
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(
-      textDisplay => textDisplay
-        .setContent(`${emojis.report} **Жалобы**
-${emojis.dot_gray} Просмотр нарушений и создание жалоб`),
-    )
-    .setButtonAccessory(btn => btn.setLabel("Смотреть").setCustomId("embed:sub-1754888253581-wld8").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.eye.id }))
+    .setButtonAccessory(btn => btn.setLabel("Перейти").setCustomId("btn:btn-8hmf0g").setStyle(ButtonStyle.Link).setEmoji({ id: emojis.bigdot_purp.id }))
   )
   .addSectionComponents(section => section
     .addTextDisplayComponents(
       textDisplay => textDisplay
-        .setContent(`${emojis.book} **Детали**
-${emojis.dot_gray} Дополнительная информация о клане`),
+        .setContent(`${emojis.report} **Жалоба #184**
+${emojis.dot_red} Тикет открыт: \`26/04/25\``),
     )
-    .setButtonAccessory(btn => btn.setLabel("Смотреть").setCustomId("embed:sub-1754931026881-i62z").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.eye.id }))
+    .setButtonAccessory(btn => btn.setLabel("Перейти").setCustomId("btn:btn-1755349286038").setStyle(ButtonStyle.Link).setEmoji({ id: emojis.bigdot_purp.id }))
+  )
+  .addSectionComponents(section => section
+    .addTextDisplayComponents(
+      textDisplay => textDisplay
+        .setContent(`${emojis.chat} **Вопрос #571**
+${emojis.dot_yell} Тикет закрыт: \`03/03/25\``),
+    )
+    .setButtonAccessory(btn => btn.setLabel("Смотреть").setCustomId("btn:btn-859w4x").setStyle(ButtonStyle.Link).setEmoji({ id: emojis.eye.id }))
+  )
+  .addSectionComponents(section => section
+    .addTextDisplayComponents(
+      textDisplay => textDisplay
+        .setContent(`${emojis.chat} **Вопрос #341**
+${emojis.dot_yell} Тикет закрыт: \`14/01/25\``),
+    )
+    .setButtonAccessory(btn => btn.setLabel("Смотреть").setCustomId("btn:btn-1755349361324").setStyle(ButtonStyle.Link).setEmoji({ id: emojis.eye.id }))
+  )
+  .addSectionComponents(section => section
+    .addTextDisplayComponents(
+      textDisplay => textDisplay
+        .setContent(`${emojis.report} **Жалоба #184**
+${emojis.dot_red} Тикет закрыт: \`18/12/24\``),
+    )
+    .setButtonAccessory(btn => btn.setLabel("Смотреть").setCustomId("btn:btn-1755349436524").setStyle(ButtonStyle.Link).setEmoji({ id: emojis.eye.id }))
+  )
+  .addActionRowComponents(row => row
+    .addComponents(
+      new ButtonBuilder().setCustomId("btn:btn-1755349547999").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.arrow_yell_left.id }).setDisabled(true),
+      new ButtonBuilder().setLabel("1/2").setCustomId("btn:btn-1755349612507").setStyle(ButtonStyle.Secondary).setDisabled(true),
+      new ButtonBuilder().setCustomId("btn:btn-1755349612940").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.arrow_yell_right.id })
+    )
   )
 await interaction.editReply({
   flags: MessageFlags.IsComponentsV2,
