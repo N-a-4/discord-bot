0a07a8b9214014ba1b9ca76aebc3b3bc8d57c3e9
N-a-4 <159144156+N-a-4@users.noreply.github.com>
2025-09-17 07:54:23 +0000

    dev 20

--- exports/history_clan_role.js
+++ exports/history_clan_role.js
@@ -48,7 +48,7 @@ ${emojis.dot_yell} Занимается добычей ресурсов для �
         .setContent(`${emojis.role_mask_mix} **Электрик**
 ${emojis.dot_yell} Обустраивает базу всеми необходимыми электрическими системами`),
     )
-    .setButtonAccessory(btn => btn.setLabel("36 дней").setCustomId("mini:mini-hfypr7z").setStyle(ButtonStyle.Secondary))
+    .setButtonAccessory(btn => btn.setLabel("36 дней").setCustomId("mini:mini-hfypr7z#2").setStyle(ButtonStyle.Secondary))
   )
   .addSectionComponents(section => section
     .addTextDisplayComponents(
@@ -56,7 +56,7 @@ ${emojis.dot_yell} Обустраивает базу всеми необходи
         .setContent(`${emojis.role_mask_mix} **Билдер**
 ${emojis.dot_yell} Отвечает за проектирование, строительство и укрепление базы клана`),
     )
-    .setButtonAccessory(btn => btn.setLabel("4 дня").setCustomId("mini:mini-hfypr7z").setStyle(ButtonStyle.Secondary))
+    .setButtonAccessory(btn => btn.setLabel("4 дня").setCustomId("mini:mini-hfypr7z#3").setStyle(ButtonStyle.Secondary))
   )
   .addSectionComponents(section => section
     .addTextDisplayComponents(
@@ -66,41 +66,10 @@ ${emojis.dot_yell} Обеспечивает клан стабильным рес
     )
     .setButtonAccessory(btn => btn.setLabel("0 дней").setCustomId("btn:btn-1757942268834").setStyle(ButtonStyle.Secondary))
   )
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(
-      textDisplay => textDisplay
-        .setContent(`${emojis.role_mask_mix} **Трубопроводчик**
-${emojis.dot_yell} Отвечает за прокладку труб и настройку всех механических систем`),
-    )
-    .setButtonAccessory(btn => btn.setLabel("0 дней").setCustomId("btn:btn-1758093916027").setStyle(ButtonStyle.Secondary))
-  )
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(
-      textDisplay => textDisplay
-        .setContent(`${emojis.role_mask_mix} **Комбат**
-${emojis.dot_yell} Основной боец клана, участвует в PvP, защищает базу, а также помогает в рейдах и вылазках`),
-    )
-    .setButtonAccessory(btn => btn.setLabel("0 дней").setCustomId("btn:btn-1758094701523").setStyle(ButtonStyle.Secondary))
-  )
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(
-      textDisplay => textDisplay
-        .setContent(`${emojis.role_mask_mix} **Пилот**
-${emojis.dot_yell} Управляет воздушным транспортом клана, доставляет бойцов, ресурсы и обеспечивает мобильность при рейдах и вылазках`),
-    )
-    .setButtonAccessory(btn => btn.setLabel("0 дней").setCustomId("btn:btn-1758094747675").setStyle(ButtonStyle.Secondary))
-  )
-  .addActionRowComponents(row => row
-    .addComponents(
-      new ButtonBuilder().setCustomId("btn:btn-1758093943148").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.arrow_yell_left.id }),
-      new ButtonBuilder().setLabel("1/2").setCustomId("btn:btn-1758093926039").setStyle(ButtonStyle.Secondary),
-      new ButtonBuilder().setCustomId("btn:btn-1758093935443").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.arrow_yell_right.id })
-    )
-  )
   .addActionRowComponents(row => row
     .addComponents(
       new ButtonBuilder().setLabel("Роли").setCustomId("btn:btn-1757942491316").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.role_mask_mix.id }),
-      new ButtonBuilder().setLabel("Должности").setCustomId("mini:mini-hfypr7z").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.case_mix.id })
+      new ButtonBuilder().setLabel("Должности").setCustomId("mini:mini-hfypr7z#4").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.case_mix.id })
     )
   )
 await interaction.editReply({
