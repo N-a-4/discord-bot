d4825700c21aa307ca593c1cf39c4895d56c7787
N-a-4 <159144156+N-a-4@users.noreply.github.com>
2025-09-18 13:56:03 +0000

    dev 20

--- exports/profile_player.js
+++ exports/profile_player.js
@@ -1,49 +1,52 @@
 const exampleContainer = new ContainerBuilder()
   .addMediaGalleryComponents(mediaGallery => mediaGallery
     .addItems(
-      new MediaGalleryItemBuilder().setURL("https://i.ibb.co/gM3ZJYGt/vidget.png")
+      new MediaGalleryItemBuilder().setURL("https://i.ibb.co/39ynLZ21/5.png")
     )
   )
+  .addActionRowComponents(row => row
+    .addComponents(
+      new ButtonBuilder().setLabel("Профиль").setCustomId("btn:b1").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.user.id }).setDisabled(true),
+      new ButtonBuilder().setLabel("Друзья").setCustomId("embed:sub-1756382998750-a1pw").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.handshake_mix.id }),
+      new ButtonBuilder().setLabel("Депозит").setCustomId("embed:sub-1756383981566-rxh9").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.money.id })
+    )
+  )
+  .addSeparatorComponents(separator => separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large))
 
     .addTextDisplayComponents(
       textDisplay => textDisplay
-        .setContent(`${emojis['24hours']} **Ежедневный бонус**
-${emojis.dot_purp} Забирайте **25**${emojis.rusticoin} ежедневно`),
+        .setContent(`${emojis.user_2_mix} **Игрок**: @username (\`.na4.\`)
+${emojis.steam_mix} **Steam**: Не подключен
+${emojis.star_yell} \`Репутация\`: 3.7 • 12 оценок
+${emojis.balance} \`Баланс\`: Скоро`),
     )
-    .setButtonAccessory(btn => btn.setLabel("Получить").setCustomId("embed:sub-1755323150368-hyj9").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.coins.id }))
+    .setThumbnailAccessory(thumb => thumb.setURL("https://images-ext-1.discordapp.net/external/Pgg89Z8fyGPWp46CwwQSh1NWFejJIU4YpsGArxYTFvc/https/cdn.discordapp.com/avatars/628606966540926978/a_50d35d8c13e85d5772bb8a8666b41452.gif?width=141&height=141"))
   
   .addSectionComponents(section => section
     .addTextDisplayComponents(
       textDisplay => textDisplay
-        .setContent(`${emojis.star_anim_yell} **Новый уровень**
-${emojis.dot_purp} Получайте РастиКоины каждый раз когда достигаете новый уровень`),
+        .setContent(`${emojis.user} **Профиль клуба**
+${emojis.dot_purp} Вы можете посмотреть как ваш профиль видят другие игроки
+`),
     )
-    .setButtonAccessory(btn => btn.setLabel("Получить").setCustomId("btn:btn-titfdx").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.coins.id }).setDisabled(true))
-  )
-  .addSectionComponents(section => section
-    .addTextDisplayComponents(
-      textDisplay => textDisplay
-        .setContent(`${emojis.discord_mix} **Вступить в [Discord](https://discord.gg/rustify)**
-${emojis.dot_purp} Станьте участником нашего комьюнити`),
-    )
-    .setButtonAccessory(btn => btn.setLabel("Получить").setCustomId("btn:btn-1758194713331").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.coins.id }).setDisabled(true))
+    .setButtonAccessory(btn => btn.setLabel("Открыть").setCustomId("btn:btn-1757277698792").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.bigdot_purp.id }))
   )
   .addSeparatorComponents(separator => separator.setDivider(true).setSpacing(SeparatorSpacingSize.Large))
   .addSectionComponents(section => section
     .addTextDisplayComponents(
       textDisplay => textDisplay
-        .setContent(`${emojis.gift_mix} **Бонусы за действия**
-${emojis.dot_purp} Выполняйте задания и зарабатывайте растикоины`),
+        .setContent(`${emojis.connect_mix} **Подключения**
+${emojis.dot_purp} Сопряжение аккаунтов с других платформ`),
     )
-    .setButtonAccessory(btn => btn.setLabel("Открыть").setCustomId("btn:btn-yce4zr").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.bigdot_purp.id }))
+    .setButtonAccessory(btn => btn.setLabel("Перейти").setCustomId("embed:sub-1756733110144-nc0r").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.more_purp.id }))
   )
   .addSectionComponents(section => section
     .addTextDisplayComponents(
       textDisplay => textDisplay
-        .setContent(`${emojis.handshake_mix} **Реферальная система**
-${emojis.dot_purp} Получай по **15**${emojis.rusticoin}за каждого друга достигнувшего 3 LvL`),
+        .setContent(`${emojis.settings} **Настройки**
+${emojis.dot_purp} Кастомизация баннера вашего профиля`),
     )
-    .setButtonAccessory(btn => btn.setLabel("Открыть").setCustomId("embed:sub-1755959804175-sl05").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.bigdot_purp.id }))
+    .setButtonAccessory(btn => btn.setLabel("Перейти").setCustomId("btn:btn-1756739185071").setStyle(ButtonStyle.Secondary).setEmoji({ id: emojis.more_purp.id }))
   )
 await interaction.editReply({
   flags: MessageFlags.IsComponentsV2,
